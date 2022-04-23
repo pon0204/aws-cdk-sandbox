70c9df4441c48100f17a4823eb55fee692defcb8
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-04-23 17:27:26 +0900

    feat: lambdaのdeploy

--- workshop/lib/workshop-stack.ts
+++ workshop/lib/workshop-stack.ts
@@ -1,19 +1,17 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 
-export class WorkshopStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+export class WorkshopStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
 
-    const queue = new sqs.Queue(this, 'WorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X, // execution environment
+      code: lambda.Code.fromAsset('lambda'), // code loaded from "lambda" directory
+      handler: 'hello.handler', // file is "hello" , function is "handler"
+    })
 
-    const topic = new sns.Topic(this, 'WorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // nothing here!
   }
 }
