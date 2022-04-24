203ab355beefeb615b6f92c599634506ae2f4181
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-04-24 10:33:40 +0900

    chore: ファイル削除

--- practice-on/lib/practice-on-stack.ts
+++ practice-on/lib/practice-on-stack.ts
@@ -1,10 +1,10 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PracticeOnStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // The code that defines your stack goes here
 
